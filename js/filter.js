c7c1212e37e3d6b6b6a8ee16e6a4a42883a309a3
jlarce <c.jose.arce@gmail.com>
2016-10-07 14:02:49 -0300

    Se agrego formulario de add

---
+++ js/filter.js
@@ -0,0 +1,14 @@
+var app = angular.module('stockApp');
+app.filter('myFormat', function() {
+    return function(x) {
+        var i, c, txt = "";
+        for (i = 0; i < x.length; i++) {
+            c = x[i];
+            if (i % 2 == 0) {
+                c = c.toUpperCase();
+            }
+            txt += c;
+        }
+        return txt;
+    };
+});
